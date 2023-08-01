49616e47025a4cffd4fb9ffaebe8916843c716e4
opq <achriste@hawaii.edu>
2023-08-01 09:39:34 -1000

    Update deps. Update publish script to use API tokens.

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 # This script builds and deploys the libwwz library to PyPi.  A valid PyPi username and password are required.
 
-
-USER=${1}
-PASS=${2}
-
 if [[ -z ${USER} ]]; then
-    echo "usage: ./run_deploy.sh <username> <password>"  >&2
-    exit 1
-fi
-
-if [[ -z ${PASS} ]]; then
-    echo "usage: ./run_deploy.sh <username> <password>"  >&2
+    echo "usage: ./run_deploy.sh <token>"  >&2
     exit 1
 fi
 
@@ -19,6 +10,8 @@ if ! [[ -x "$(command -v twine)" ]]; then
   echo 'Error: twine is not installed.' >&2
   exit 1
 fi
+
+TOKEN=${1}
 
 set -o nounset
 set -o errexit
@@ -28,7 +21,7 @@ cd ..
 
 #python3 setup.py sdist bdist_wheel
 python3 -m build .
-twine upload -r pypi -u ${USER} -p ${PASS} --skip-existing dist/*
+twine upload -r pypi -u __token__ -p ${TOKEN} --skip-existing dist/*
 
 VERSION="v$(python -c 'import toml; print(toml.load("pyproject.toml")["project"]["version"])')"
 git tag -a ${VERSION} -m"Release ${VERSION}"
